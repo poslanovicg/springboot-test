edf9d4f5e783bfcefaae3e91c024e85feee8ac1f
Gabriel Poslanovic <gabriel.poslanovic@austriacard.at>
2025-03-05 11:03:28 +0100

    finished setup

--- src/main/java/com/example/demo/config/DatabaseConfig.java
+++ src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -3,24 +3,33 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
-
-    @Value("${database.url}")
-    private String url;
-
-    @Value("${database.username}")
-    private String username;
-
-    @Value("${database.password}")
-    private String password;
+    @Bean
+    @Profile("dev")
+    public DataSource devDataSource(
+            @Value("${database.url}") String url,
+            @Value("${database.username}") String username,
+            @Value("${database.password}") String password) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        return dataSource;
+    }
 
     @Bean
-    public DataSource dataSource() {
+    @Profile("docker")
+    public DataSource dockerDataSource(
+            @Value("${docker.database.url}") String url,
+            @Value("${docker.database.username}") String username,
+            @Value("${docker.database.password}") String password) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUrl(url);
         dataSource.setUsername(username);
